015f460324fd5eb11f32b53873824551a297b3d3
Dimetriu <dima.rashidov@gmail.com>
2018-04-01 22:37:22 +0300

    change routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,10 @@
 Rails.application.routes.draw do
-  get '/:locale' => 'home#index'
-
   root 'home#index'
-  
   scope '/:locale' do
 
     devise_for :admins
     devise_for :users
 
-  end
-  
+  end  
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
 end
